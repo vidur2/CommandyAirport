400673aa7bc9c9dd9c628563022cfc8ee50c731a
Jeffrey Xu <jeffreyxu44@gmail.com>
2023-02-22 09:55:46 -0500

    airlines update delete

--- src/AirlineTable.java
+++ src/AirlineTable.java
@@ -1,3 +1,4 @@
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.HashMap;
 
@@ -6,16 +7,15 @@ import java.util.HashMap;
 
 public class AirlineTable extends Table<String> {
 
-    
     public AirlineTable(HashMap<String, WidgetRecord<String>> results, ResultSet rs) {
         super(results);
-        //TODO Auto-generated constructor stub
+        // TODO Auto-generated constructor stub
     }
 
     @Override
     public void mapDatabase() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
@@ -44,14 +44,51 @@ public class AirlineTable extends Table<String> {
 
     @Override
     public String editRecord(WidgetRecord<String> w) {
-        // TODO Auto-generated method stub
-        return null;
+        // Creating SQL Query
+        String query = "UPDATE airline SET ein=? WHERE id=?;";
+
+        try {
+
+            // Prepared statements parameterize and protect against SQL injection attacks
+            PreparedStatement preparedStatement = this.conn.prepareStatement(query);
+
+            // Putting in fields for SQL Query
+            preparedStatement.setString(1, w.getEIN());
+            preparedStatement.setInt(2, w.getID());
+
+            // Execute SQL Query
+            preparedStatement.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+        // loadResults(); -> for GUI
+        return "Airline Record Updated";
     }
 
     @Override
     public String deleteRecord(WidgetRecord<String> w) {
-        // TODO Auto-generated method stub
-        return null;
+        // Creating SQL Query
+        String query = "DELETE FROM airline WHERE id=?;";
+
+        try {
+
+            // Putting in fields for SQL Query
+            PreparedStatement preparedStatement = this.conn.prepareStatement(query);
+            preparedStatement.setInt(1, w.getID());
+
+            // Executing SQL Query
+            preparedStatement.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+        // loadResults(); -> for GUI
+        return "Airline Record Deleted.";
+    }
+
     }
 
     @Override
@@ -59,5 +96,5 @@ public class AirlineTable extends Table<String> {
         // TODO Auto-generated method stub
         return 0;
     }
-    
+
 }
